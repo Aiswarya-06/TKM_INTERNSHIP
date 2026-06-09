dee86d624e56e1142c87394e8c880973b8a92b03
Aiswarya-06 <aiswaryakrishnavilasamsalini@gmail.com>
2026-06-09 22:09:49 +0530

    Create sr_ff.v

---
+++ day2/SR_flipflop/design/sr_ff.v
@@ -0,0 +1,38 @@
+module sr_ff(
+    input s,
+    input r,
+    input rst,
+    input clk,
+    output reg q,
+    output reg qbar
+);
+
+// Trigger strictly on the rising edge of the clock
+always @(posedge clk) begin
+    if (rst) begin
+        q    <= 1'b0;
+        qbar <= 1'b1;
+    end
+    else begin
+        case ({s, r})
+            2'b00: begin 
+                q    <= q; 
+                qbar <= qbar; 
+            end
+            2'b01: begin 
+                q    <= 1'b0; 
+                qbar <= 1'b1; 
+            end
+            2'b10: begin 
+                q    <= 1'b1; 
+                qbar <= 1'b0; 
+            end
+            2'b11: begin 
+                q    <= 1'bx; 
+                qbar <= 1'bx; 
+            end
+        endcase
+    end
+end
+
+endmodule
