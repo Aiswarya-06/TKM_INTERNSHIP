b56a2f5a419c5437269d4b2822bd2e1be9f760f2
Aiswarya-06 <aiswaryakrishnavilasamsalini@gmail.com>
2026-06-10 09:59:21 +0530

    Update d_fftb.v

--- day2/d_flipflop/tb/d_fftb.v
+++ day2/d_flipflop/tb/d_fftb.v
@@ -1,1 +1,20 @@
-
+module d_ff_tb();
+reg d_tb;
+reg clk_tb;
+wire q_tb;
+integer m;
+d_flip dut(d_tb,clk_tb,q_tb);//instantiate
+initial
+begin
+{d_tb,clk_tb}=0;
+end
+always #5 clk_tb=~clk_tb;
+initial
+begin
+for(m=0;m<4;m=m+1)begin
+{d_tb}=m;
+#10;
+end
+$monitor("the value of q_tb is %b",q_tb);
+end
+endmodule
