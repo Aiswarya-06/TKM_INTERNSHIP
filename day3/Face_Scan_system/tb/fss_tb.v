85f3caec3b8a0feee0911f3fa12985de89c3bf38
Aiswarya-06 <aiswaryakrishnavilasamsalini@gmail.com>
2026-06-11 09:32:43 +0530

    Update fss_tb.v

--- day3/Face_Scan_system/tb/fss_tb.v
+++ day3/Face_Scan_system/tb/fss_tb.v
@@ -1,1 +1,31 @@
 
+module face_detect_tb;
+
+reg clk_tb;
+reg rst_tb;
+reg [7:0] s_in_tb;
+wire [7:0] d_out_tb;
+
+top dut(clk_tb,rst_tb,s_in_tb,d_out_tb);
+
+initial
+begin
+clk_tb=0;
+forever #5 clk_tb=~clk_tb;
+end
+initial
+begin
+rst_tb=1;
+s_in_tb=8'h00;
+#20;
+rst_tb=0;
+
+#10 s_in_tb=8'h12;
+#10 s_in_tb=8'h34;
+#10 s_in_tb=8'h56;
+#10 s_in_tb=8'h78;
+
+#50;
+end
+
+endmodule
