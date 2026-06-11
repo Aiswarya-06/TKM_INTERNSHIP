f9bce1ebfba7a2e3d94ed004f8fe6b937f9c2e84
Aiswarya-06 <aiswaryakrishnavilasamsalini@gmail.com>
2026-06-11 07:10:32 +0530

    Update face_mod.v

--- day3/Face_Scan_system/design/face_mod.v
+++ day3/Face_Scan_system/design/face_mod.v
@@ -1,4 +1,4 @@
-module Face_Mod(input clk,input [7:0]s_in ,output reg [7:0]s_out);
+module face_Mod(input clk,input [7:0]s_in ,output reg [7:0]s_out);
 always @(posedge clk)
 s_out<=s_in;
 endmodule
