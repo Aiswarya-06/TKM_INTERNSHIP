b5e7fc810d781a140a83f27e552c496d527be866
Aiswarya-06 <aiswaryakrishnavilasamsalini@gmail.com>
2026-06-20 22:29:39 +0530

    Update status_register.sv

--- proj/status_register.sv
+++ proj/status_register.sv
@@ -1,3 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.06.2026 11:34:42
+// Design Name: 
+// Module Name: status_register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module status_register (
     input  logic clk,
     input  logic rst_n,
@@ -43,23 +64,21 @@ always_comb begin
     next_state = state;
 
     case (state)
-        IDLE:  next_state = RELU1;
+        IDLE  : next_state = RELU1;
 
-        RELU1: if (relu_done) next_state = POOL1;
+        RELU1 : if (relu_done) next_state = POOL1;
 
-        POOL1: if (pool_done) next_state = RELU2;
+        POOL1 : if (pool_done) next_state = RELU2;
 
-        RELU2: if (relu_done) next_state = POOL2;
+        RELU2 : if (relu_done) next_state = POOL2;
 
-        POOL2: if (pool_done) next_state = FC1;
+        POOL2 : if (pool_done) next_state = FC1;
 
-        FC1:   if (fc_done)   next_state = FC2;
+        FC1   : if (fc_done) next_state = FC2;
 
-        FC2:   if (fc_done)   next_state = DONE;
+        FC2   : if (fc_done) next_state = DONE;
 
-        DONE:  next_state = DONE;
-
-        default: next_state = IDLE;
+        DONE  : next_state = IDLE; // go back to IDLE after done
     endcase
 end
 
@@ -67,20 +86,27 @@ end
 // OUTPUT LOGIC
 ///////////////////////////////////////////////////////
 always_comb begin
-    relu_en    = 0;
-    pool_en    = 0;
-    fc_en      = 0;
-    frame_done = 0;
+    relu_en = 0;
+    pool_en = 0;
+    fc_en   = 0;
 
     case (state)
-        RELU1, RELU2: relu_en    = 1;
-
-        POOL1, POOL2: pool_en    = 1;
-
-        FC1, FC2:     fc_en      = 1;
-
-        DONE:         frame_done = 1;
+        RELU1, RELU2: relu_en = 1;
+        POOL1, POOL2: pool_en = 1;
+        FC1, FC2    : fc_en   = 1;
     endcase
+end
+
+///////////////////////////////////////////////////////
+// FRAME DONE PULSE (1 CLOCK ONLY)
+///////////////////////////////////////////////////////
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        frame_done <= 0;
+    else if (state == FC2 && fc_done)
+        frame_done <= 1;
+    else
+        frame_done <= 0;
 end
 
 endmodule
